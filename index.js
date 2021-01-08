3ba6fd8a31b1d8bb1918799ecc4651ab8ac527b1
Brandon <brandonbgrayson@gmail.com>
2021-01-07 21:39:16 -0500

    added runSearch function and switch statement for user responses

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const Employee = require('./lib/employee')
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
@@ -15,28 +16,60 @@ var connection = mysql.createConnection({
   database: "employee_DB"
 });
 
-// generate an array of questions
-const questions = [
-    {
-        type: 'list',
-        name: 'options',
-        message: 'What would you like to do?',
-        choices: [
-            'Add An Employee',
-            'Add a role',
-            'Add a Department',
-            'View departments',
-            'View roles',
-            'View employees',
-            'Update employee roles'
-        ]
-    }
-]
-inquirer.prompt(questions).then(answers => {
-    console.log(answers)
-})
-
 connection.connect(function(err) {
     if (err) throw err;
+    runSearch()
     console.log('connection made')
 });
+
+function runSearch() {
+    inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        'Add An Employee',
+        'Add a role',
+        'Add a Department',
+        'View departments',
+        'View roles',
+        'View employees',
+        'Update employee roles'
+      ]
+    })
+    .then(function(answer) {
+        switch (answer.action) {
+        case "Add An Employee":
+          addEmployee();
+          break;
+  
+        // case "Add a role":
+        //   multiSearch();
+        //   break;
+  
+        // case "Add a Department":
+        //   rangeSearch();
+        //   break;
+  
+        // case "View departments":
+        //   songSearch();
+        //   break;
+  
+        // case "View roles":
+        //   connection.end();
+        //   break;
+
+        //   case "View employees":
+        //     multiSearch();
+        //     break;
+    
+        //   case "Update employee roles":
+        //     rangeSearch();
+        //     break;
+        }
+      });
+}
+
+
+// generate an array of questions
